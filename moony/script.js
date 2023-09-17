01ed589a3ec8b2df3f0f3c221f405d134bdf2153
Szu-Yu Chen <achaoswing@gmail.com>
2023-09-16 22:04:29 -0700

    [moony] Mobile compatible

--- moony/script.js
+++ moony/script.js
@@ -1,25 +1,34 @@
 const sections = document.querySelectorAll('.section');
 let currentSection = 0;
+let isScrolling = false;
 
 function scrollToSection(index) {
     sections[index].scrollIntoView({ behavior: 'smooth' });
 }
 
-window.addEventListener('wheel', (e) => {
-    if (e.deltaY > 0) {
-        // Scrolling down
-        if (currentSection < sections.length - 1) {
-            currentSection++;
-            scrollToSection(currentSection);
-        }
-    } else {
-        // Scrolling up
-        if (currentSection > 0) {
-            currentSection--;
-            scrollToSection(currentSection);
-        }
+function handleScroll() {
+    if (!isScrolling) {
+        isScrolling = true;
+        setTimeout(() => {
+            isScrolling = false;
+            const scrollPosition = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+            const windowHeight = window.innerHeight;
+            const sectionHeight = sections[currentSection].offsetHeight;
+
+            if (scrollPosition >= currentSection * sectionHeight + windowHeight / 2) {
+                currentSection++;
+                if (currentSection < sections.length) {
+                    scrollToSection(currentSection);
+                }
+            } else if (scrollPosition < currentSection * sectionHeight - windowHeight / 2 && currentSection > 0) {
+                currentSection--;
+                scrollToSection(currentSection);
+            }
+        }, 100); // Adjust the delay if needed
     }
-});
+}
+
+window.addEventListener('scroll', handleScroll);
 
 window.addEventListener('DOMContentLoaded', () => {
     scrollToSection(currentSection);
